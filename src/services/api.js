4a29cb7c65bc688c5eedd81c0342b6ede07a2faf
Helena <helenapl145@gmail.com>
2024-03-05 18:16:03 -0300

    add new url backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "https://api-notes-production-f794.up.railway.app"
+    baseURL: "https://api-notes-production-a935.up.railway.app"
 });
- //https://api-notes-production-f794.up.railway.app
  ///http://localhost:3333
